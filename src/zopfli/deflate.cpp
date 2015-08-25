a18c62d4a2e9ed18b5834c3f33db5f3dd11bbf70
fhanau <fhanau@web.de>
2015-08-25 18:49:57 +0200

    Fix OptimizeHuffmanForRle()

--- src/zopfli/deflate.cpp
+++ src/zopfli/deflate.cpp
@@ -422,8 +422,10 @@ static void OptimizeHuffmanForRle(unsigned length, size_t* counts) {
 
   /* 1) We don't want to touch the trailing zeros. We may break the
   rules of the format by adding more data in the distance codes. */
-  for (; length != 0; --length) {
-    if (counts[length - 1] != 0) {
+  for (;; --length) {
+    if (length == 0) {
+      return;
+    }    if (counts[length - 1] != 0) {
       /* Now counts[0..length - 1] does not have trailing zeros. */
       break;
     }
