92b8502332a5b99a0e17ae8b88a21457312a81a2
fhanau <fhanau@web.de>
2015-09-13 21:46:12 +0200

    Tiny compression tweak

--- src/zopfli/util.c
+++ src/zopfli/util.c
@@ -229,7 +229,7 @@ void ZopfliInitOptions(ZopfliOptions* options, unsigned mode, unsigned multithre
     }
   }
   options->num = mode < 3 ? 3 : 9;
-  options->skipdynamic = mode > 3 ? 80 : 200;
+  options->skipdynamic = mode > 3 ? 80 : 180;
   options->blocksplittingmax = mode > 2 || isPNG ? 0 : multithreading > 15 ? multithreading : 15;
   options->trystatic = mode > 2 ? 800 : 0;
   options->multithreading = multithreading;
