b00a682b3e02a57c632281e27853785eaa9b0b70
fhanau <fhanau@web.de>
2016-10-24 11:29:28 +0200

    Fix compilation

--- src/zlib/zlib.h
+++ src/zlib/zlib.h
@@ -38,7 +38,7 @@ extern "C" {
 #endif
 
 #define ZLIB_VERSION "1.2.8-hanaumods"
-#define ZLIB_VERNUM 0x128f
+#define ZLIB_VERNUM 0x1280
 #define ZLIB_VER_MAJOR 1
 #define ZLIB_VER_MINOR 2
 #define ZLIB_VER_REVISION 8
