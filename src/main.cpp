6a28d0454182e7d2bd38125c0cd355045d2efc40
fhanau <fhanau@web.de>
2017-03-28 22:07:15 +0200

    Bump version

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ static void Usage() {
     printf (
             "Efficient Compression Tool\n"
             "(c) 2014-2016 Felix Hanau.\n"
-            "Version 0.6"
+            "Version 0.7"
 #ifdef __DATE__
             " compiled on %s\n"
 #endif
